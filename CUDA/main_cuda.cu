982552e6031b9a62f92801e28213d359e41bb3c3
nipunmeegoda <nipun.meegoda@gmail.com>
2025-12-06 05:08:54 +0530

    latest

--- CUDA/main_cuda.cu
+++ CUDA/main_cuda.cu
@@ -1,7 +1,9 @@
+%%writefile main_cuda.cu
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <cuda_runtime.h>
+
+#define CHECK_CUDA(call) do { if ((call) != cudaSuccess) exit(1); } while (0)
 
 __constant__ float d_kernel[9] = {
     1.0f/16, 2.0f/16, 1.0f/16,
@@ -9,143 +11,122 @@ __constant__ float d_kernel[9] = {
     1.0f/16, 2.0f/16, 1.0f/16
 };
 
-unsigned char* readPGM(const char* filename, int* width, int* height, int* maxVal)
-{
+unsigned char* readPGM(const char* filename, int* w, int* h, int* maxVal) {
     FILE* fp = fopen(filename, "rb");
     if (!fp) exit(1);
 
     char magic[3] = {0};
-    fscanf(fp, "%2s", magic);
+    if (fscanf(fp, "%2s", magic) != 1) exit(1);
+    if (magic[0] != 'P' || magic[1] != '5') exit(1);
 
     int c = fgetc(fp);
     while (c == '#') { while (fgetc(fp) != '\n'); c = fgetc(fp); }
     ungetc(c, fp);
 
-    fscanf(fp, "%d %d", width, height);
-    fscanf(fp, "%d", maxVal);
+    if (fscanf(fp, "%d %d", w, h) != 2) exit(1);
+    if (fscanf(fp, "%d", maxVal) != 1) exit(1);
     fgetc(fp);
 
-    int size = (*width) * (*height);
+    int size = (*w) * (*h);
     unsigned char* img = (unsigned char*)malloc(size);
-    fread(img, 1, size, fp);
-    fclose(fp);
+    if (!img) exit(1);
 
+    if (fread(img, 1, size, fp) != (size_t)size) exit(1);
+    fclose(fp);
     return img;
 }
 
-void writePGM(const char* filename, const unsigned char* img,
-              int width, int height, int maxVal)
-{
+void writePGM(const char* filename, const unsigned char* img, int w, int h, int maxVal) {
     FILE* fp = fopen(filename, "wb");
     if (!fp) exit(1);
-    fprintf(fp, "P5\n%d %d\n%d\n", width, height, maxVal);
-    fwrite(img, 1, width * height, fp);
+    fprintf(fp, "P5\n%d %d\n%d\n", w, h, maxVal);
+    fwrite(img, 1, w * h, fp);
     fclose(fp);
 }
 
-// Gaussian Blur CUDA Kernel
-__global__ void gaussianBlurKernel(const unsigned char* input,
-                                   unsigned char* output,
-                                   int width, int height)
-{
+__global__ void gaussianBlurKernel(const unsigned char* in,
+                                   unsigned char* out,
+                                   int w, int h) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (x >= width || y >= height) return;
+    if (x >= w || y >= h) return;
 
     float sum = 0.0f;
-
     for (int ky = -1; ky <= 1; ky++) {
         for (int kx = -1; kx <= 1; kx++) {
-
             int px = x + kx;
             int py = y + ky;
-
             if (px < 0) px = 0;
-            if (px >= width) px = width - 1;
+            if (px >= w) px = w - 1;
             if (py < 0) py = 0;
-            if (py >= height) py = height - 1;
+            if (py >= h) py = h - 1;
 
-            size_t idx = (size_t)py * (size_t)width + (size_t)px;
-
-            float pixel  = (float)input[idx];
+            size_t idx = (size_t)py * (size_t)w + (size_t)px;
+            float pixel  = (float)in[idx];
             float weight = d_kernel[(ky + 1) * 3 + (kx + 1)];
             sum += pixel * weight;
         }
     }
 
-    if (sum < 0.0f) sum = 0.0f;
+    if (sum < 0.0f)   sum = 0.0f;
     if (sum > 255.0f) sum = 255.0f;
 
-    output[(size_t)y * width + x] = (unsigned char)(sum + 0.5f);
+    size_t outIdx = (size_t)y * (size_t)w + (size_t)x;
+    out[outIdx] = (unsigned char)(sum + 0.5f);
 }
 
-int main(int argc, char* argv[])
-{
-    if (argc < 5) {
-        printf("Usage: %s input.pgm output.pgm blockSize passes\n", argv[0]);
-        return 1;
-    }
+int main(int argc, char* argv[]) {
+    if (argc < 3) return 1;
 
     const char* inFile  = argv[1];
     const char* outFile = argv[2];
 
-    int blockSize = atoi(argv[3]);   
-    int passes    = atoi(argv[4]);   
-    if (blockSize <= 0) blockSize = 16;
-    if (passes <= 0) passes = 1;
+    int blockSize = (argc >= 4) ? atoi(argv[3]) : 16;
+    if (blockSize < 1) blockSize = 16;
+    if (blockSize * blockSize > 1024) blockSize = 32;
 
-    printf("Block size: %d x %d\n", blockSize, blockSize);
-    printf("Passes: %d\n", passes);
+    int passes = (argc >= 5) ? atoi(argv[4]) : 1;
+    if (passes < 1) passes = 1;
 
-    int width, height, maxVal;
-    unsigned char* h_input = readPGM(inFile, &width, &height, &maxVal);
-
-    int size = width * height;
-    unsigned char* h_output = (unsigned char*)malloc(size);
+    int w, h, maxVal;
+    unsigned char* h_in  = readPGM(inFile, &w, &h, &maxVal);
+    int size = w * h;
+    unsigned char* h_out = (unsigned char*)malloc(size);
+    if (!h_out) exit(1);
 
     unsigned char *d_in, *d_out;
-    cudaMalloc(&d_in, size);
-    cudaMalloc(&d_out, size);
-
-    cudaMemcpy(d_in, h_input, size, cudaMemcpyHostToDevice);
+    CHECK_CUDA(cudaMalloc(&d_in,  size));
+    CHECK_CUDA(cudaMalloc(&d_out, size));
+    CHECK_CUDA(cudaMemcpy(d_in, h_in, size, cudaMemcpyHostToDevice));
 
     dim3 block(blockSize, blockSize);
-    dim3 grid((width  + block.x - 1) / block.x,
-              (height + block.y - 1) / block.y);
+    dim3 grid((w + block.x - 1) / block.x, (h + block.y - 1) / block.y);
 
     cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start);
+    CHECK_CUDA(cudaEventCreate(&start));
+    CHECK_CUDA(cudaEventCreate(&stop));
 
+    CHECK_CUDA(cudaEventRecord(start));
     for (int i = 0; i < passes; i++) {
-        gaussianBlurKernel<<<grid, block>>>(d_in, d_out, width, height);
-        cudaDeviceSynchronize();
-
-        unsigned char* tmp = d_in;
-        d_in = d_out;
-        d_out = tmp;
+        gaussianBlurKernel<<<grid, block>>>(d_in, d_out, w, h);
+        CHECK_CUDA(cudaDeviceSynchronize());
+        unsigned char* tmp = d_in; d_in = d_out; d_out = tmp;
     }
+    CHECK_CUDA(cudaEventRecord(stop));
+    CHECK_CUDA(cudaEventSynchronize(stop));
 
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
+    float ms = 0.0f;
+    CHECK_CUDA(cudaEventElapsedTime(&ms, start, stop));
+    printf("%.6f ms (block %d, passes %d)\n", ms, blockSize, passes);
 
-    float ms = 0;
-    cudaEventElapsedTime(&ms, start, stop);
+    CHECK_CUDA(cudaMemcpy(h_out, d_in, size, cudaMemcpyDeviceToHost));
+    writePGM(outFile, h_out, w, h, maxVal);
 
-    printf("Total GPU time for %d passes = %.3f ms (%.3f sec)\n",
-           passes, ms, ms / 1000.0f);
-
-    cudaMemcpy(h_output, d_out, size, cudaMemcpyDeviceToHost);
-
-    writePGM(outFile, h_output, width, height, maxVal);
-
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
     cudaFree(d_in);
     cudaFree(d_out);
-    free(h_input);
-    free(h_output);
-
-    printf("Output saved: %s\n", outFile);
+    free(h_in);
+    free(h_out);
     return 0;
 }
